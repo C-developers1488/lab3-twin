6f6a6fa89772db812413ea247648788b22909820
LastGuardian89 <dmitriy-bobrovskiy03@mai.ru>
2021-11-29 10:15:51 +0300

    test ver 4

--- tests/test.cpp
+++ tests/test.cpp
@@ -1,46 +1,76 @@
-// Copyright 2021 C-developers1488
+// Copyright by C-developers1488
 
 #include <gtest/gtest.h>
 #include "header.hpp"
 
 TEST(Example, EmptyTest) {
   EXPECT_TRUE(true);
-  int intNum = 73;
-  double doubleNum = 50.5;
-  auto *intNumPtr = new int(intNum);
-  auto *doubleNumPtr = new double(doubleNum);
-  auto intSharedPtr = SharedPtr<int>(intNumPtr);
-  auto doubleSharedPtr = SharedPtr<double>(doubleNumPtr);
-  EXPECT_EQ(intSharedPtr.use_count(), 1);
-
-  EXPECT_EQ(intSharedPtr.get(), intNumPtr);
-  EXPECT_EQ(doubleSharedPtr.get(), doubleNumPtr);
-
-  auto intSharedPtrCopy = intSharedPtr;
-  auto doubleSharedPtrCopy = doubleSharedPtr;
-  EXPECT_EQ(static_cast<bool>(intSharedPtrCopy), true);
-  EXPECT_EQ(*doubleSharedPtrCopy, doubleNum);
-
-  auto intSharedPtrMove = SharedPtr<int>(std::move(intSharedPtrCopy));
-  EXPECT_EQ(*intSharedPtrMove, intNum);
-  EXPECT_EQ(static_cast<bool>(intSharedPtrMove), true);
-  EXPECT_EQ(intSharedPtrMove.operator->(), intNumPtr);
-
-  intSharedPtrMove.reset();
-  EXPECT_EQ(static_cast<bool>(intSharedPtrMove), false);
-
-  int newInt = 10;
-  auto *newIntPtr = new int(newInt);
-  intSharedPtr.reset(newIntPtr);
-  EXPECT_EQ(static_cast<bool>(intSharedPtr), true);
-  EXPECT_EQ(intSharedPtr.get(), newIntPtr);
-  EXPECT_EQ(*intSharedPtr, newInt);
-
-  intSharedPtr.swap(intSharedPtrMove);
-  EXPECT_EQ(static_cast<bool>(intSharedPtr), false);
-  EXPECT_EQ(static_cast<bool>(intSharedPtrMove), true);
-  EXPECT_EQ(*intSharedPtrMove, newInt);
-  EXPECT_EQ(intSharedPtrMove.get(), newIntPtr);
-  EXPECT_EQ(intSharedPtrMove.operator->(), newIntPtr);
-  EXPECT_EQ(intSharedPtr.operator->(), nullptr);
+}
+TEST(Test, Zerro) {
+  SharedPtr<int> sp;
+  EXPECT_EQ((sp),0);
+}
+TEST(Test, UseCount) {
+  int val = 5;
+  SharedPtr<int> sp1 (&val);
+  SharedPtr<int> sp2(sp1);
+  SharedPtr<int> sp3(sp2);
+  EXPECT_EQ(sp1.use_count(),3);
+}
+TEST(Test, Clas_Strelka) {
+  class Values{
+   public:
+    int val;
+  };
+  Values val1;
+  val1.val=20;
+  SharedPtr<Values> sp5(&val1);
+  EXPECT_EQ(sp5->val,20);
+}
+TEST(Test, Zvezda) {
+  int val = 5, val2 = 30;
+  SharedPtr<int> sp1 (&val);
+  SharedPtr<int> sp2(sp1);
+  SharedPtr<int> sp3(sp2);
+  SharedPtr<int> spoz(&val2);
+  std::stringstream str;
+  str<<*sp1<<" "<<*sp2<<" "<<*sp3<<" "<<*spoz;
+  std::string stroka = "5 5 5 30";
+  EXPECT_EQ(str.str(), stroka);
+}
+TEST(Test, Get) {
+  int val = 5, val2 = 30;
+  SharedPtr<int> sp1 (&val);
+  SharedPtr<int> sp2(sp1);
+  SharedPtr<int> sp3(sp2);
+  SharedPtr<int> spoz(&val2);
+  std::stringstream str, str2;
+  str<<*sp1.get()<<" "<<*sp2.get()<<" "<<*sp3.get()<<" "<<*spoz.get();
+  std::string stroka = "5 5 5 30";
+  EXPECT_EQ(str.str(), stroka);
+}
+TEST(Test, Reset) {
+  int val = 5, val2 =30;
+  SharedPtr<int> sp1 (&val);
+  SharedPtr<int> sp2(sp1);
+  SharedPtr<int> sp3(sp2);
+  sp1.reset();
+  sp2.reset(&val2);
+  std::stringstream str;
+  str<<(sp1)<<" "<<*sp2;
+  std::string stroka = "0 30";
+  EXPECT_EQ(str.str(), stroka);
+}
+TEST(Test, Swap) {
+  int val = 5, val2 = 30;
+  SharedPtr<int> sp1 (&val);
+  SharedPtr<int> sp2(sp1);
+  SharedPtr<int> sp3(sp2);
+  SharedPtr<int> spoz(&val2);
+  std::stringstream str;
+  str<<*sp3<<" "<<*spoz;
+  sp3.swap(spoz);
+  str<<" "<<*sp3<<" "<<*spoz;
+  std::string stroka = "5 30 30 5";
+  EXPECT_EQ(str.str(), stroka);
 }
